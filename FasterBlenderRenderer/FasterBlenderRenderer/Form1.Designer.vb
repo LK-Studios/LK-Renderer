90889673f5d56394d857028d4baf13bcb73bc925
LK Studios <lkkoch@outlook.com>
2015-12-31 18:23:05 +0100

    Upload

---
+++ FasterBlenderRenderer/FasterBlenderRenderer/Form1.Designer.vb
@@ -0,0 +1,371 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar4 = New System.Windows.Forms.TrackBar()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(15, 215)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(272, 42)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 196)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Your .blend File "
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(15, 422)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(262, 20)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Animation(Image preview not available)"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox2.Location = New System.Drawing.Point(15, 345)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(107, 20)
+        Me.CheckBox2.TabIndex = 5
+        Me.CheckBox2.Text = "Single Frame"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(8, 371)
+        Me.TrackBar1.Maximum = 200
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(272, 45)
+        Me.TrackBar1.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(11, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Settings:"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(15, 791)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(278, 39)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "RENDER"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(15, 263)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(163, 24)
+        Me.Button3.TabIndex = 22
+        Me.Button3.Text = "Select"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"2.76", "2.75", "2.74", "2.73", "2.72", "2.71", "2.70", "2.69", "2.68", "2.67", "2.66", "2.65 ", "2.64", "2.63", "2.62", "2.61", "2.60"})
+        Me.ComboBox1.Location = New System.Drawing.Point(15, 48)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(272, 26)
+        Me.ComboBox1.TabIndex = 24
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 16)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Blender-Version "
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(15, 80)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(278, 39)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Download"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(15, 154)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(278, 39)
+        Me.Button5.TabIndex = 27
+        Me.Button5.Text = "Show Downloaded Versions"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 125)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(278, 23)
+        Me.ProgressBar1.TabIndex = 28
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(345, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(960, 540)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 29
+        Me.PictureBox1.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(15, 670)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(163, 24)
+        Me.Button4.TabIndex = 32
+        Me.Button4.Text = "Select"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 590)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 16)
+        Me.Label4.TabIndex = 31
+        Me.Label4.Text = "Outputpath"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(15, 622)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(272, 42)
+        Me.TextBox2.TabIndex = 30
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(15, 740)
+        Me.TrackBar2.Maximum = 32
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(272, 45)
+        Me.TrackBar2.TabIndex = 34
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 721)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(292, 16)
+        Me.Label5.TabIndex = 35
+        Me.Label5.Text = "# of Threads(leave at 0 for # of processor cores)"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Cycles", "Internal Blender Renderer"})
+        Me.ComboBox2.Location = New System.Drawing.Point(15, 318)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(269, 21)
+        Me.ComboBox2.TabIndex = 36
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 290)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(98, 16)
+        Me.Label6.TabIndex = 37
+        Me.Label6.Text = "Render Engine"
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(8, 464)
+        Me.TrackBar3.Maximum = 999
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Size = New System.Drawing.Size(272, 45)
+        Me.TrackBar3.TabIndex = 38
+        '
+        'TrackBar4
+        '
+        Me.TrackBar4.Location = New System.Drawing.Point(8, 542)
+        Me.TrackBar4.Maximum = 999
+        Me.TrackBar4.Name = "TrackBar4"
+        Me.TrackBar4.Size = New System.Drawing.Size(272, 45)
+        Me.TrackBar4.TabIndex = 39
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 445)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 16)
+        Me.Label7.TabIndex = 40
+        Me.Label7.Text = "Start Frame"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(12, 512)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(74, 16)
+        Me.Label8.TabIndex = 41
+        Me.Label8.Text = "End Frame"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1311, 841)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TrackBar4)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents TrackBar4 As TrackBar
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ FasterBlenderRenderer/FasterBlenderRenderer/Form1.vb
@@ -0,0 +1,180 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Public Class Form1
+    Dim Renderstring As String
+    Dim appdata As String = My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\LKrenderer"
+    Dim Blenderversion As String
+    Dim Downloadversion As String
+    Dim rendering As Boolean = False
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        CheckBox2.Text = "Single Frame (Framme number: " & TrackBar1.Value & ")"
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If System.IO.Directory.Exists(appdata) = False Then
+            System.IO.Directory.CreateDirectory(appdata)
+            System.IO.Directory.CreateDirectory(appdata & "\BV")
+            System.IO.Directory.CreateDirectory(appdata & "\RenderedPics")
+            Dim fs As FileStream = File.Create(appdata & "\DownloadedVersions.txt")
+            Dim fs1 As FileStream = File.Create(appdata & "\readme.txt")
+            fs.Close()
+            fs1.Close()
+            My.Computer.FileSystem.WriteAllText(appdata & "\readme.txt",
+            "This is the directory where everything for LK-Renderer is stored. " & vbCrLf &
+            "############   Please do not modify any Files here    ##############" &
+            "############   If you modify anything here programm may not functon anymore  #########", True)
+        End If
+    End Sub
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            CheckBox1.Checked = False
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("After you hit the Button a Command-line Window will show up and render your image." & vbCrLf & "This window will automaticly be closen after your render has finished!")
+        If ComboBox2.Text = "Cycles" Then
+            If CheckBox2.Checked = True Then
+                Renderstring = "blender.exe -b " & TextBox1.Text & " -E CYCLES" & " -o " & TextBox2.Text & "\Renderdpic_###.png" & " -t " & TrackBar2.Value & " -f " & TrackBar1.Value
+            End If
+            If CheckBox1.Checked = True Then
+                Renderstring = "blender.exe -b " & TextBox1.Text & " -E CYCLES" & " -o " & TextBox2.Text & "\Renderdpic_###.png" & " -s " & TrackBar3.Value & " -e " & TrackBar4.Value & " -t " & TrackBar2.Value & " -a "
+            End If
+        End If
+        If ComboBox2.Text = "Internal Blender Renderer" Then
+            If CheckBox2.Checked = True Then
+                Renderstring = "blender.exe -b " & TextBox1.Text & " -E BLENDER_RENDER" & " -o " & TextBox2.Text & "\Renderdpic_###.png" & " -t " & TrackBar2.Value & " -f " & TrackBar1.Value
+            End If
+            If CheckBox1.Checked = True Then
+                Renderstring = "blender.exe -b " & TextBox1.Text & " -E BLENDER_RENDER" & " -o " & TextBox2.Text & "\Renderdpic_###.png" & " -s " & TrackBar3.Value & " -e " & TrackBar4.Value & " -t " & TrackBar2.Value & " -a "
+            End If
+        End If
+        Try
+            Shell(appdata & "\BV_R\blender-" & ComboBox1.SelectedItem & "-windows64\" & Renderstring)
+            rendering = True
+            Timer1.Enabled = True
+        Catch ex As Exception
+            MsgBox("Error:" & ex.Message)
+        End Try
+    End Sub
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            CheckBox2.Checked = False
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("Select the .blend file you want To use!")
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.InitialDirectory = "c:\"
+        openFileDialog1.Filter = ".blend Files (*.blend)|*.blend|All files (*.*)|*.*"
+        openFileDialog1.FilterIndex = 1
+        openFileDialog1.RestoreDirectory = True
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = openFileDialog1.FileName
+        End If
+    End Sub
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim fileReader As String
+            fileReader = My.Computer.FileSystem.ReadAllText(appdata & "\DownloadedVersions.txt")
+            If InStr(fileReader, ComboBox1.SelectedItem) = 0 Then
+                MsgBox("Starting to download Blender " & ComboBox1.SelectedItem & vbCrLf & "Window might seem inactive but in the Background the Blender-Version is being downloaded!" & vbCrLf & "When an error occurs a messagebox will pop up when the Blender-Version was downloaded a messagebox will pop up!")
+                Downloadversion = "http://download.blender.org/release/Blender" & ComboBox1.SelectedItem & "/blender-" & ComboBox1.SelectedItem & "-windows64.zip"
+                download(Downloadversion, appdata & "\BV\" & ComboBox1.SelectedItem & ".zip")
+            End If
+            If InStr(fileReader, ComboBox1.SelectedItem) > 0 Then
+                MsgBox("The selected Blender-Version has already been Downloaded")
+            End If
+        Catch ex As Exception
+            MsgBox("Error " & ex.Message)
+        End Try
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText(appdata & "\DownloadedVersions.txt")
+        MsgBox(fileReader)
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim FolderBrowser As New FolderBrowserDialog
+        FolderBrowser.Description = "Select an outputpath for the image"
+        FolderBrowser.ShowNewFolderButton = True
+        FolderBrowser.SelectedPath = My.Computer.FileSystem.SpecialDirectories.Desktop
+        If FolderBrowser.ShowDialog = DialogResult.OK Then
+            TextBox2.Text = FolderBrowser.SelectedPath
+        End If
+    End Sub
+    Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
+        Label5.Text = "# of threads (" & TrackBar2.Value & ") 0 for # of processor cores"
+    End Sub
+    Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
+        Label7.Text = "Start Frame (" & TrackBar3.Value & ")"
+    End Sub
+    Private Sub TrackBar4_Scroll(sender As Object, e As EventArgs) Handles TrackBar4.Scroll
+        Label8.Text = "End Frame (" & TrackBar4.Value & ")"
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If CheckBox2.Checked = True Then
+            Try
+                Dim framea As String = TrackBar1.Value
+                If framea < 100 Then
+                    framea = "0" & TrackBar1.Value
+                End If
+                If framea < 10 Then
+                    framea = "00" & TrackBar1.Value
+                End If
+                PictureBox1.Image = System.Drawing.Bitmap.FromFile(TextBox2.Text & "\Renderdpic_" & framea & ".png")
+                Timer1.Enabled = False
+            Catch ex As Exception
+            End Try
+        End If
+        Dim currentframe As Integer = TrackBar3.Value
+        Dim frame As String = currentframe
+        If CheckBox1.Checked = True Then
+            frame = currentframe
+            If frame < 100 Then
+                frame = "0" & currentframe
+            End If
+            If frame < 10 Then
+                frame = "00" & currentframe
+            End If
+            Try
+                PictureBox1.Image = System.Drawing.Bitmap.FromFile(TextBox2.Text & "\Renderdpic_" & frame & ".png")
+                currentframe = currentframe + 1
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+    Sub download(myLink As String, myFile As String)
+        Try
+            Me.Text = "Downloading..."
+            Label1.Text = "Downloading..."
+            Dim myRequest As System.Net.WebRequest = System.Net.WebRequest.Create(myLink)
+            Dim myResponse As System.Net.WebResponse = myRequest.GetResponse()
+            Dim myStream As System.IO.Stream = myResponse.GetResponseStream()
+            Dim myReader As New System.IO.BinaryReader(myStream)
+            Dim myFileStream As New System.IO.FileStream(myFile, System.IO.FileMode.Create)
+            Dim i As Long
+            ProgressBar1.Maximum = myResponse.ContentLength
+            ProgressBar1.Value = 0
+            For i = 1 To myResponse.ContentLength
+                myFileStream.WriteByte(myReader.ReadByte())
+                If ProgressBar1.Value + 1 < ProgressBar1.Maximum Then
+                    ProgressBar1.Value = ProgressBar1.Value + 1
+                End If
+            Next i
+            myFileStream.Flush()
+            myFileStream.Close()
+            MsgBox("Downloaded")
+            Me.Text = "Finished downloading"
+            MsgBox("Starting to unpack now...")
+            Me.Text = "Unpacking..."
+            Label1.Text = "Unpacking..."
+            System.IO.Compression.ZipFile.ExtractToDirectory(appdata & "\BV\" & ComboBox1.SelectedItem & ".zip", appdata & "\BV_R\")
+            MsgBox("Unpacked Blender-Version " & ComboBox1.SelectedItem & "! Now ready to use")
+            My.Computer.FileSystem.WriteAllText(appdata & "\DownloadedVersions.txt",
+           ComboBox1.SelectedItem & vbCrLf, True)
+            Me.Hide()
+        Catch ex As Exception
+            MsgBox("Error:  " & ex.Message)
+        End Try
+    End Sub
+End Class
