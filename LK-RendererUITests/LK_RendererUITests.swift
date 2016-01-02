ce36e70703243f7b31da0493b49ceff4aee4c656
ArdiMaster <ardimaster@outlook.de>
2016-01-02 15:04:13 +0100

    Changed license to the MIT license due to change of the license of the original Windows version of the LK-Renderer

--- LK-RendererUITests/LK_RendererUITests.swift
+++ LK-RendererUITests/LK_RendererUITests.swift
@@ -4,22 +4,10 @@
 //
 //  Created by Adrian Welcker on 31.12.15.
 //
-//  LK-Renderer
 //  Copyright (C) 2016 LK-Studios
 //
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License along
-//  with this program; if not, write to the Free Software Foundation, Inc.,
-//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//  Distributed under the terms and conditions of the MIT license.
+//  You can obtain a copy of the license at https://opensource.org/licenses/MIT
 //
 
 import XCTest
